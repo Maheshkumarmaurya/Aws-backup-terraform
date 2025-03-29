db94b384992abac4502baaa6eb6996f491006a09
Maheshkumarmaurya <maheshmaurya817284@gmail.com>
2025-03-29 21:15:06 +0530

    deleted

--- outputs.tf
+++
@@ -1,7 +0,0 @@
-output "backup_plan_id" {
-  value = aws_backup_plan.ec2_backup_plan.id
-}
-
-output "backup_selection_id" {
-  value = aws_backup_selection.ec2_selection.id
-}
--- terraform.tfvars
+++
@@ -1,1 +0,0 @@
-ec2_instance_id = "i-06c350761f2526172"  # Replace with your EC2 ID
